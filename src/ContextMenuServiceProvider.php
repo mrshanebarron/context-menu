6ec87f270a3ad44058656abd91c824ab834d3e0a
Shane Barron <shane@Mac.hsd1.fl.comcast.net>
2025-12-15 18:58:08 -0500

    Fix PHP syntax - use PascalCase for all class names

--- src/ContextMenuServiceProvider.php
+++ src/ContextMenuServiceProvider.php
@@ -3,14 +3,36 @@
 namespace MrShaneBarron\ContextMenu;
 
 use Illuminate\Support\ServiceProvider;
+use MrShaneBarron\ContextMenu\Livewire\ContextMenu;
+use MrShaneBarron\ContextMenu\View\Components\ContextMenu as BladeContextMenu;
 
 class ContextMenuServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/sb-context-menu.php', 'sb-context-menu');
+    }
+
     public function boot(): void
     {
-        if (class_exists(\Livewire\Livewire::class)) {
-            \Livewire\Livewire::component('sb-context-menu', Livewire\ContextMenu::class);
-        }
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'sb-context-menu');
+
+        if (class_exists(\Livewire\Livewire::class)) {
+            \Livewire\Livewire::component('sb-context-menu', ContextMenu::class);
+        }
+
+        $this->loadViewComponentsAs('ld', [
+            BladeContextMenu::class,
+        ]);
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/sb-context-menu.php' => config_path('sb-context-menu.php'),
+            ], 'sb-context-menu-config');
+
+            $this->publishes([
+                __DIR__ . '/../resources/views' => resource_path('views/vendor/sb-context-menu'),
+            ], 'sb-context-menu-views');
+        }
     }
 }
